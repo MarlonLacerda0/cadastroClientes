033a8444b85a7a763b3f3c48d7d08c2c2d560474
Marlon <marlonlac10@hotmail.com>
2026-01-17 13:13:01 -0300

    Criada a model, banco, tabela e fazendo a conexão com EF

---
+++ cadastroClientes/Migrations/20260117160511_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cadastroClientes.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DatNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DatCadastro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
---
+++ cadastroClientes/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace cadastroClientes.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? CPF { get; set; }
+        public string? Telefone { get; set; }
+        public DateTime DatNascimento { get; set; }
+        public DateTime DatCadastro { get; set; }        
+    }
+}
